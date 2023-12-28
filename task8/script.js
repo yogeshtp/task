1ebe2191a725d23e0994b5d8be6d84c67feaa6b1
Yogesh Yadav <yogesh.yadav@teknopoint.local>
2023-12-28 11:43:38 +0530

    first commit

---
+++ task8/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.carousel').slick({
+    slidesToShow: 3,
+    dots:true,
+    centerMode: true,
+    });
+  });
+  
---
+++ task9/js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+  $(".customer__testimonial__carousel").slick({
+    infinite: true,
+    slidesToShow: 3,
+    dots: true,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
